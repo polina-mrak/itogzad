5c989b17fc7a5dbc833bde79c36d1946dc50a9f9
Polina Shimonchuk <shimonchuk2@gmail.com>
2023-04-29 14:27:53 +0700

    changes 1 task and change slider

---
+++ js/funcindex.js
@@ -0,0 +1,12 @@
+function qwe() {
+    document.getElementById('maincolor')
+    maincolor.style.backgroundColor = "#438cb0"
+}
+function qwe1() {
+    document.getElementById('maincolor')
+    maincolor.style.backgroundColor = " #0f1314"
+}
+function qwe2() {
+    document.getElementById('maincolor')
+    maincolor.style.backgroundColor = " #162e39"
+}
